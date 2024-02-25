60362c8930addd157419f100a6987c289836ccba
Cho Phan <callmechophan@gmail.com>
2024-02-25 23:40:30 +0700

    1.3.4

--- main.cpp
+++ main.cpp
@@ -107,8 +107,24 @@ void ProcessUserOption(int user_option) {
 }
 
 int main() {
-    OrderBooks order {7.44564869, 1, "2020/03/17 17:01:24.884492", "ETH/BTC", OrderBook::bid};
-    std::cout << order.amount << std::endl;
+    std::vector<OrderBooks> orders;
+
+    OrderBooks order {7.44564869, 0.02187308, "2020/03/17 17:01:24.884492", "ETH/BTC", OrderBook::bid};
+
+    orders.push_back(order);
+
+    // three iterate over a vector
+    for (OrderBooks& order : orders) {
+        std::cout << order.amount << std::endl;
+    }
+
+    for (unsigned int i = 0; i < orders.size(); ++i) {
+        std::cout << orders[i].amount << std::endl;
+    }
+
+    for (unsigned int i = 0; i < orders.size(); ++i) {
+        std::cout << orders.at(i).amount << std::endl;
+    }
 
     // while (true) {
     //     PrintMenu();
