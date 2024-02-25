56b8ed0b5b6abe4fec16261d2018677c889252a2
Cho Phan <callmechophan@gmail.com>
2024-02-25 15:05:12 +0700

    1.6.1

--- main.cpp
+++ main.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
 
+void PrintMenu() {
+    std::cout << "\nChoose an option:" << std::endl;
+    std::cout << "1. Print help" << std::endl;
+    std::cout << "2. Print exchange stats" << std::endl;
+    std::cout << "3. Make an offer" << std::endl;
+    std::cout << "4. Make a bid" << std::endl;
+    std::cout << "5. Print wallet" << std::endl;
+    std::cout << "6. Continue" << std::endl;
+    std::cout << "==========" << std::endl;
+}
+
+int GetUserOption() {
+    int user_option;
+    std::cin >> user_option;
+    std::cout << "You chose: " << user_option << std::endl;
+
+    return user_option;
+}
+
+void ProcessUserOption(int user_option) {
+    if (user_option == 0) { // bad input
+        std::cout << "Invalid choice. Re-input!" << std::endl;
+    }
+}
+
+
 int main() {
     while (true) {
-        std::cout << "Choose an option:" << std::endl;
-        std::cout << "1. Print help" << std::endl;
-        std::cout << "2. Print exchange stats" << std::endl;
-        std::cout << "3. Make an offer" << std::endl;
-        std::cout << "4. Make a bid" << std::endl;
-        std::cout << "5. Print wallet" << std::endl;
-        std::cout << "6. Continue" << std::endl;
-        std::cout << "==========" << std::endl;
+        PrintMenu();
 
-        int user_option;
-        std::cin >> user_option;
-        std::cout << "You chose: " << user_option << std::endl;
+        int user_option = GetUserOption();
 
-        if (user_option == 0) { // bad input
-            std::cout << "Invalid choice. Re-input" << std::endl;
-        }
+        ProcessUserOption(user_option);
     }
+
+    return 0;
 }
